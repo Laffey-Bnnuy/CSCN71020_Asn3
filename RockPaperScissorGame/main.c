e51cd52b3ba7672838a749435f3b4ae9dd3ea08a
Nhat Minh Nguyen <mahirooyamalaffey@gmail.com>
2024-10-27 15:18:39 -0400

    Fix the mistake to use the for loop to check the Invalid condition

--- RockPaperScissorGame/main.c
+++ RockPaperScissorGame/main.c
@@ -5,14 +5,12 @@ int main(void) {
 }
 bool Game(char* Input1, char* Input2, char* Result) {
 
-	char StringCase[3][STRING_MAX_SIZE] = { "Rock", "Paper","Scissor" };
-	for (int i = 0; i < 3; i++) {
-		if (Input1 != StringCase[i] || Input2 != StringCase[i]) {
-			strncpy(Result, "Invalid", STRING_MAX_SIZE);
-			return false;
-		}
+	if ((Input1 != "Rock" && Input1 != "Paper" && Input1 !="Scissor") 
+		|| (Input2 != "Rock" && Input2 != "Paper" && Input2 != "Scissor")) {
+		strncpy(Result, "Invalid", STRING_MAX_SIZE);
+		return false;
 	}
-
+	
 	if (Input1 == Input2) {
 		strncpy(Result, "Draw", STRING_MAX_SIZE);
 	}
