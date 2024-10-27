eed24e863ba4da4b2a19e4693bd786529bb773db
Nhat Minh Nguyen <mahirooyamalaffey@gmail.com>
2024-10-27 15:08:04 -0400

    Fix string declare bug

--- RockPaperScissorGame/main.c
+++ RockPaperScissorGame/main.c
@@ -1,16 +1,17 @@
 #include "main.h"
-
+#define STRING_MAX_SIZE 8
 int main(void) {
-	char Result[7] = "";
+	char Result[STRING_MAX_SIZE] = "";
 	Game("Rock", "Scissor", Result);
 	return 0;
 }
 void Game(char* Input1, char* Input2, char* Result) {
 	if (Input1 == Input2) {
-		strncpy(Result, "Draw", 7);
+		strncpy(Result, "Draw", STRING_MAX_SIZE);
 	}
 	else if (Input1 == "Rock") {
 		if (Input2 =="Scissor")
-			strncpy(Result, "Player1", 7);
+			strncpy(Result, "Player1", STRING_MAX_SIZE);
+		else strncpy(Result, "Player2", STRING_MAX_SIZE);
 	}
 }
